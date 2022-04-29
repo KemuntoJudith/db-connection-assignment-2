aa3bf2fc0aa4bb5d8894ad188811b9219d7124ea
JUDITH <JUDITH@DESKTOP-9CCJU8N>
2022-04-29 13:19:57 +0300

    db connection

--- src/handyman/system/dbConnection.java
+++ src/handyman/system/dbConnection.java
@@ -10,9 +10,12 @@ package handyman.system;
  * @author JUDITH
  */
 import java.sql.*;
+import java.io.*; 
 
 
 public class dbConnection {
+    
+    Connection con;
     
 public void getConnection(){
     
@@ -22,7 +25,7 @@ public void getConnection(){
     
     //Step 2: Establish connection
     Connection con = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/HandyManSystem", "root", "");
+            "jdbc:mysql://localhost:3306/HandyMan System", "root", "");
             
     //Check if the connection is successful
     if (con != null) {
@@ -33,7 +36,7 @@ public void getConnection(){
             }       
     
     //Step 3: Create a statement for executing sql statements
-    //Here HandyManSystem is  database name, root is username and no password
+    //Here HandyMan System is  database name, root is username and no password
     
     Statement stmt = con.createStatement();
     
@@ -41,12 +44,32 @@ public void getConnection(){
     ResultSet rs = stmt.executeQuery("Select * from logins");
     
     //Step 5: Process resulltset
-    
     while (rs.next()) {
         System.out.println("Username is " + rs.getString("Username"));
     }
    
-    //Step 6: Close the connection
+    //Step 6: Insert a new row to logins table and process the result
+    int result = stmt.executeUpdate("insert into logins(userName,Password,Role) values('jn1982','pass1234','Manager')");
+    
+    if (result > 0)
+				System.out.println("successfully inserted");
+
+			else
+				System.out.println(
+					"unsucessful insertion ");
+    // Output 
+    /*Connection Successful
+    Username is jn1982
+    successfully inserted
+    BUILD SUCCESSFUL (total time: 0 seconds)*/
+    
+    //Step 7: Delete a row from the table
+     String sql = "DELETE FROM logins " + "WHERE uSerName = jn1982";
+        int executeUpdate = stmt.executeUpdate(sql);
+         while(rs.next())
+    
+    
+    //Step 8: Close the connection
     con.close();    
     }   
 
